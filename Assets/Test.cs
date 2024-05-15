7582bc7e2ddaacefd2169cc5d2c878a4b16edbe1
json.ante <antematis@users.noreply.github.com>
2024-05-16 02:14:56 +0900

    コンソール上に変数の値を表示する
    Test.cs上の変数scoreの値をコンソール上に表示するようにコードの書き換え

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,8 +7,12 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //コンソール上にHello, Worldと表示する
-        Debug.Log("Hello, World");
+        //変数scoreを宣言
+        int score;
+        //scoreに値を格納
+        score = 150;
+        //コンソール上に変数scoreの値を表示
+        Debug.Log(score);
     }
 
     // Update is called once per frame
