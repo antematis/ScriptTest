4fb616ae6d62cc9b9e930bfa742b31393645908c
json.ante <antematis@users.noreply.github.com>
2024-05-16 02:19:41 +0900

    2つの変数の加算値を格納した変数を表示
    Test.cs上に変数aと変数bの加算した値を格納した変数cの値をコンソール上に表示する

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,12 +7,13 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //変数scoreを宣言
-        int score;
-        //scoreに値を格納
-        score = 150;
-        //コンソール上に変数scoreの値を表示
-        Debug.Log(score);
+        //変数を初期化
+        int a = 3;
+        int b = 4;
+        //変数aと変数bの加算した値を変数cに格納
+        int c = a + b;
+        //変数cの値を表示する
+        Debug.Log(c);
     }
 
     // Update is called once per frame
