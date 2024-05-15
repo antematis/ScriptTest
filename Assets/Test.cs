253e40e49dc358590bb55cfda1f301f50e62d3bf
json.ante <antematis@users.noreply.github.com>
2024-05-16 02:38:18 +0900

    三項演算子により変数valに格納する値を決める
    変数valの値を変数numが1である場合-100をそうでない場合には100を変数valに格納する

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,24 +7,12 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //変数moneyを50で初期化
-        int money = 100;
-        //moneyの値が50以下の場合
-        if(money <= 50)
-        {
-            Debug.Log("武器を売る");
-        }
-        //moneyが200以上の場合
-        else if(money >= 200)
-        {
-            Debug.Log("武器を買う");
-        }
-        //変数moneyの値が上記以外の場合
-        else
-        {
-
-            Debug.Log("ポーションを買う");
-        }
+        //numを1で初期化
+        int num = 1;
+        //valに格納する値を条件によって変える
+        int val = (num == 1) ? -100 : 100;
+        //valの値を表示する
+        Debug.Log(val);
     }
 
     // Update is called once per frame
