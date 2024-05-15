79045bb3ef3f3e8d39579d89468c6d4dfe011278
json.ante <antematis@users.noreply.github.com>
2024-05-16 02:27:31 +0900

    moneyの値によるif~else文
    変数moneyの値が100未満の場合のelse句によりコンソール上に「武器を売る」を表示する

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,12 +7,17 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //変数moneyを200で初期化
-        int money = 200;
+        //変数moneyを50で初期化
+        int money = 50;
         //moneyの値が100以上の場合、「ポーションを買う」を表示する
         if(money >= 100)
         {
             Debug.Log("ポーションを買う");
+        }
+        //moneyが100未満の場合、「武器を売る」を表示する
+        else
+        {
+            Debug.Log("武器を売る");
         }
     }
 
