7ffe8412eab174c39d05a99ffe1fe8f703d42571
json.ante <antematis@users.noreply.github.com>
2024-05-16 15:39:50 +0900

    void関数Hello()
    引数なしで呼び出し時にコンソール上に「Hello, UnityChan」と表示する

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,23 +4,17 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    //「Hello, UnityChan」と表示する関数
+    void Hello()
+    {
+        Debug.Log("Hello, UnityChan");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        //配列を初期化
-        int[] position = { 30, 20, 50, 10, 80, 15, 60, 100 };
-
-        //配列の要素数だけ処理を繰り返す
-        for(int i = 0; i < position.Length; i++)
-        {
-            //配列の要素が50以上の場合
-            if (position[i] >= 50)
-            {
-                //配列の要素を表示する
-                Debug.Log(position[i]);
-            }
-        }
-
+        //Hello関数を呼び出す
+        Hello();
     }
 
     // Update is called once per frame
