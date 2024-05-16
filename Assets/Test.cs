4470cd4adcd601ee803f3da606dd792275fccfd2
json.ante <antematis@users.noreply.github.com>
2024-05-16 15:52:33 +0900

    int関数Add(int a, int b)
    int関数Add()は引数a、bの整数値を渡し、変数cに引数aとbの加算した結果の値を変数cに格納し、変数cの値をint関数Add()は返す。関数Start()上で関数Add()の返り値の結果をコンソール上に表示する

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    //「Hello」に続けて引数の文字列を表示する関数
-    void HelloName(string name)
+    //第一引数と第二引数の値を加算した値を返す関数
+    int Add(int a, int b)
     {
-        Debug.Log("Hello, " + name);
+        //第一引数と第二引数の値を加算して、変数cに格納する
+        int c = a + b;
+        //変数cを呼び出し元の関数に返す
+        return c;
     }
     // Start is called before the first frame update
     void Start()
     {
-        //Hello関数に「Unity」という引数を渡す
-        HelloName("Unity");
+        //Add関数に「3」と「6」の引数を渡し、返り値をnum変数に格納
+        int num = Add(3, 6);
+        //numを表示する
+        Debug.Log(num);
     }
 
     // Update is called once per frame
